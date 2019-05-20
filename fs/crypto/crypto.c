484b5b7693d0f9a4ff2d24cd987b633eb4de7363
Eric Biggers <ebiggers@google.com>
2019-05-20 09:29:42 -0700

    fscrypt: clean up some BUG_ON()s in block encryption/decryption
    [ Upstream commit eeacfdc68a104967162dfcba60f53f6f5b62a334 ]
    
    Replace some BUG_ON()s with WARN_ON_ONCE() and returning an error code,
    and move the check for len divisible by FS_CRYPTO_BLOCK_SIZE into
    fscrypt_crypt_block() so that it's done for both encryption and
    decryption, not just encryption.
    
    Reviewed-by: Chandan Rajendra <chandan@linux.ibm.com>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/crypto/crypto.c
+++ fs/crypto/crypto.c
@@ -158,7 +158,10 @@ int fscrypt_do_page_crypto(const struct inode *inode, fscrypt_direction_t rw,
 	sdp_fs_command_t *cmd = NULL;
 #endif
 
-	BUG_ON(len == 0);
+	if (WARN_ON_ONCE(len <= 0))
+		return -EINVAL;
+	if (WARN_ON_ONCE(len % FS_CRYPTO_BLOCK_SIZE != 0))
+		return -EINVAL;
 
 	BUILD_BUG_ON(sizeof(iv) != FS_IV_SIZE);
 	BUILD_BUG_ON(AES_BLOCK_SIZE != FS_IV_SIZE);
@@ -281,6 +284,7 @@ struct page *fscrypt_encrypt_page(const struct inode *inode,
 	struct page *ciphertext_page = page;
 	int err;
 
+<<<<<<< HEAD
 #if defined(CONFIG_CRYPTO_DISKCIPHER_DEBUG)
 	if (__fscrypt_inline_encrypted(inode))
 		crypto_diskcipher_debug(FS_ENC_WARN, 0);
@@ -295,6 +299,8 @@ struct page *fscrypt_encrypt_page(const struct inode *inode,
 
 	BUG_ON(len % FS_CRYPTO_BLOCK_SIZE != 0);
 
+=======
+>>>>>>> 457b9eb24342... fscrypt: clean up some BUG_ON()s in block encryption/decryption
 	if (inode->i_sb->s_cop->flags & FS_CFLG_OWN_PAGES) {
 		/* with inplace-encryption we just encrypt the page */
 		err = fscrypt_do_page_crypto(inode, FS_ENCRYPT, lblk_num, page,
@@ -306,7 +312,8 @@ struct page *fscrypt_encrypt_page(const struct inode *inode,
 		return ciphertext_page;
 	}
 
-	BUG_ON(!PageLocked(page));
+	if (WARN_ON_ONCE(!PageLocked(page)))
+		return ERR_PTR(-EINVAL);
 
 	ctx = fscrypt_get_ctx(inode, gfp_flags);
 	if (IS_ERR(ctx))
@@ -354,6 +361,7 @@ EXPORT_SYMBOL(fscrypt_encrypt_page);
 int fscrypt_decrypt_page(const struct inode *inode, struct page *page,
 			unsigned int len, unsigned int offs, u64 lblk_num)
 {
+<<<<<<< HEAD
 #if defined(CONFIG_CRYPTO_DISKCIPHER_DEBUG)
 	if (__fscrypt_inline_encrypted(inode))
 		crypto_diskcipher_debug(FS_DEC_WARN, 0);
@@ -366,6 +374,11 @@ int fscrypt_decrypt_page(const struct inode *inode, struct page *page,
 		return 0;
 	}
 #endif
+=======
+	if (WARN_ON_ONCE(!PageLocked(page) &&
+			 !(inode->i_sb->s_cop->flags & FS_CFLG_OWN_PAGES)))
+		return -EINVAL;
+>>>>>>> 457b9eb24342... fscrypt: clean up some BUG_ON()s in block encryption/decryption
 
 	return fscrypt_do_page_crypto(inode, FS_DECRYPT, lblk_num, page, page,
 				      len, offs, GFP_NOFS);
