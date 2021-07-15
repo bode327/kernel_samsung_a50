f27446dc98929a91c90b899eeeabf43d3c73e9d1
¨Bode327¨ <cristovive.eucreio@gmail.com>
2021-07-15 09:46:24 -0300

    Upstream to 4.14.124

---
+++ git_clean.sh
@@ -0,0 +1,7 @@
+git rm --cached -r -f ./out
+git rm --cached -r -f ./output
+git rm --cached -r -f ./GCC_11.X
+git rm --cached -r -f ./Optimized
+git rm --cached -r -f ./A50.sh
+git rm --cached -r -f ./make.sh
+git rm --cached -r -f ./clang
